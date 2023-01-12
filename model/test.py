d916de2d284821911df07719bbbe50df3dac4eb8
KunWang-22 <651877220@qq.com>
2023-01-12 16:23:10 +0800

    add valiadation for test

--- model/test.py
+++ model/test.py
@@ -60,24 +60,62 @@ def test_new(args, device):
 
 
 def test(args, device):
-    test_dataset = Dataset_UKDA(args.file_path, args.aggregation_num, args.mode, args.test_user, args.test_day)
-    test_dataloader = DataLoader(test_dataset, len(test_dataset), shuffle=True)
+    test_dataset = Dataset_UKDA(args.file_path, args.aggregation_num, args.mode, args.test_user, args.test_day, args.validation_num)
+    test_dataloader = DataLoader(test_dataset, len(test_dataset), shuffle=False)
 
     model = Model(args.input_dim, args.condition_input_dim, args.embedding_dim, args.num_head, args.num_layer, args.num_block, args.dropout, device).to(device)
     model.load_state_dict(torch.load("../log/model/model.pt"))
-    print("Load model successfully !!!")
+    print("Load Model Successfully !!!")
     diffusion = Diffusion(noise_step=args.noise_step, beta_start=args.beta_start, beta_end=args.beta_end, data_length=args.data_length, device=device)
 
     print("Start Test !!!")
-    for i, (data, condition) in enumerate(test_dataloader):
-        condition = condition.to(device)
-        predicted = diffusion.sample(model, len(test_dataloader), condition)
+    repeat_type = "once"
+    if args.mode == "test":
+        for i, (data, condition) in enumerate(test_dataloader):
+            condition = condition.to(device)
+            predicted = diffusion.sample(model, len(test_dataset), condition)
 
-        reverse_real_data = reverse_data(data.squeeze(-1).reshape(args.test_user, -1), test_dataset.scaler.mean_[-args.test_user:].reshape(-1,1), test_dataset.scaler.scale_[-args.test_user:].reshape(-1,1))
-        reverse_generated_data = reverse_data(predicted.cpu().detach().squeeze(-1).reshape(args.test_user, -1), test_dataset.scaler.mean_[-args.test_user:].reshape(-1,1), test_dataset.scaler.scale_[-args.test_user:].reshape(-1,1))
+            reverse_real_data = reverse_data(data.squeeze(-1).reshape(args.test_user, -1), test_dataset.scaler.mean_[-args.test_user:].reshape(-1,1), test_dataset.scaler.scale_[-args.test_user:].reshape(-1,1))
+            reverse_generated_data = reverse_data(predicted.cpu().detach().squeeze(-1).reshape(args.test_user, -1), test_dataset.scaler.mean_[-args.test_user:].reshape(-1,1), test_dataset.scaler.scale_[-args.test_user:].reshape(-1,1))
 
-    np.save("../result/real_data.npy", reverse_real_data.reshape(args.test_user, args.test_day, -1).numpy())
-    np.save("../result/generated_data.npy", reverse_generated_data.reshape(args.test_user, args.test_day, -1).numpy())
+            real_data = reverse_real_data.reshape(args.test_user, args.test_day, -1).numpy()
+            generated_data = reverse_generated_data.reshape(args.test_user, args.test_day, -1).numpy()
+
+        if args.repeat == 0:
+            print("Test Once !!!")
+        else:
+            print("Test with Repeat !!!")
+            repeat_type = "repeat"
+            for _ in range(args.repeat-1):
+                for i, (data, condition) in enumerate(test_dataloader):
+                    condition = condition.to(device)
+                    predicted = diffusion.sample(model, len(test_dataset), condition)
+
+                    reverse_real_data = reverse_data(data.squeeze(-1).reshape(args.test_user, -1), test_dataset.scaler.mean_[-args.test_user-args.validation_num:-args.validation_num].reshape(-1,1), test_dataset.scaler.scale_[-args.test_user-args.validation_num:-args.validation_num].reshape(-1,1))
+                    reverse_generated_data = reverse_data(predicted.cpu().detach().squeeze(-1).reshape(args.test_user, -1), test_dataset.scaler.mean_[-args.test_user-args.validation_num:-args.validation_num].reshape(-1,1), test_dataset.scaler.scale_[-args.test_user-args.validation_num:-args.validation_num].reshape(-1,1))
+
+                    real_data_temp = reverse_real_data.reshape(args.test_user, args.test_day, -1).numpy()
+                    generated_data_temp = reverse_generated_data.reshape(args.test_user, args.test_day, -1).numpy()
+
+                    real_data = np.concatenate((real_data, real_data_temp), axis=-1)
+                    generated_data = np.concatenate((generated_data, generated_data_temp), axis=-1)
+            
+            real_data.reshape(args.test_user, args.test_day, -1, 48)
+            generated_data.reshape(args.test_user, args.test_day, -1, 48)
+        
+    elif args.mode == "validation":
+        for i, (data, condition) in enumerate(test_dataloader):
+            condition = condition.to(device)
+            predicted = diffusion.sample(model, len(test_dataset), condition)
+
+            reverse_real_data = reverse_data(data.squeeze(-1).reshape(args.validation_num, -1), test_dataset.scaler.mean_[-args.validation_num:].reshape(-1,1), test_dataset.scaler.scale_[-args.validation_num:].reshape(-1,1))
+            reverse_generated_data = reverse_data(predicted.cpu().detach().squeeze(-1).reshape(args.validation_num, -1), test_dataset.scaler.mean_[-args.validation_num:].reshape(-1,1), test_dataset.scaler.scale_[-args.validation_num:].reshape(-1,1))
+
+            real_data = reverse_real_data.reshape(args.validation_num, 365, -1).numpy()
+            generated_data = reverse_generated_data.reshape(args.validation_num, 365, -1).numpy()
+
+    np.save("../result/real_data_"+args.mode+"_"+repeat_type+".npy", real_data)
+    np.save("../result/generated_data_"+args.mode+"_"+repeat_type+".npy", generated_data)
 
 
 
@@ -86,9 +124,10 @@ if __name__ == "__main__":
     # Dataset 参数
     parser.add_argument("--file_path", type=str, default="../data/UKDA_2013_clean.csv")
     parser.add_argument("--aggregation_num", type=int, default=10)
-    parser.add_argument("--mode", type=str, default="test")
+    parser.add_argument("--mode", type=str, default="train")
     parser.add_argument("--test_user", type=int, default=10)
-    parser.add_argument("--test_day", type=int, default=30)
+    parser.add_argument("--test_day", type=int, default=60)
+    parser.add_argument("--validation_num", type=int, default=6)
     # Diffusion 参数
     parser.add_argument("--noise_step", type=int, default=50)
     parser.add_argument("--beta_start", type=float, default=0.0001)
@@ -102,10 +141,12 @@ if __name__ == "__main__":
     parser.add_argument("--num_layer", type=int, default=6)
     parser.add_argument("--num_block", type=int, default=6)
     parser.add_argument("--dropout", type=float, default=0.2)
-    
+    # Training 参数
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument('--num_epoch', type=int, default=100)
     parser.add_argument('--learning_rate', type=float, default=0.001)
+    # Test 参数
+    parser.add_argument("--repeat", type=int, default=0)
 
     parser.add_argument('--cuda', action='store_true', default=False)
     args = parser.parse_args()
